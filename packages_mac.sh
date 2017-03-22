b2c98420c60ca695c49be155108d70dc5f69e2fd
HFTBattle Team <team@hftbattle.com>
2017-03-22 18:13:50 +0300

    Automated repository update; build 81.

--- packages_mac.sh
+++ packages_mac.sh
@@ -8,7 +8,7 @@ echo "Installing Homebrew"
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 echo "Homebrew has been installed"
 
-echo "Installing CMake and Python3"
-brew install cmake python3
+echo "Installing CMake, JsonCpp and Python3"
+brew install cmake jsoncpp python3
 
 echo "Eveything is installed"
