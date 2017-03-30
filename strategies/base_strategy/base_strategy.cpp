12c5873a2baece8bfc5cb4543498fcd07367ec9f
HFTBattle Team <team@hftbattle.com>
2017-03-30 21:51:33 +0300

    Automated repository update; build 89.

--- strategies/base_strategy/base_strategy.cpp
+++ strategies/base_strategy/base_strategy.cpp
@@ -13,28 +13,28 @@ public:
     set_max_total_amount(max_pos_);
   }
 
-  Amount amount_available(const Amount pos, const Dir dir) {
-    auto max_volume = std::min(max_pos_ - dir_sign(dir) * pos, volume_);
+  Amount amount_available(Amount pos, Dir dir) {
+    Amount max_volume = std::min(max_pos_ - dir_sign(dir) * pos, volume_);
     return std::max(0, max_volume);
   }
 
   void trading_book_update(const OrderBook& order_book) override {
     const auto& orders = order_book.orders();
-    auto middle_price = order_book.middle_price();
-    auto pos = executed_amount();
+    Price middle_price = order_book.middle_price();
+    Amount pos = executed_amount();
 
     add_chart_point("middle_price", middle_price);
 
     for (Dir dir : {BID, ASK}) {
-      auto target_price = middle_price - dir_sign(dir) * offset_;
-      auto order_amount = amount_available(pos, dir);
+      Price target_price = middle_price - dir_sign(dir) * offset_;
+      Amount order_amount = amount_available(pos, dir);
 
       if (orders.active_orders_count(dir) == 0) {
         if (order_amount > 0) {
           add_limit_order(dir, target_price, order_amount);
         }
       } else {
-        auto current_order = orders.orders_by_dir(dir).front();
+        Order* current_order = orders.orders_by_dir(dir).front();
         if (current_order->price() != target_price) {
           delete_order(current_order);
           if (order_amount > 0) {
